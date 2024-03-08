56368e3bcf988a3d796833628f0ea784833186a0
itzmeproman <atulpandey3030@gmail.com>
2024-03-08 09:15:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/HarshalPurohitEdits/TheMovieProviderBot.git /TheMovieProviderBot
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TheMovieProviderBot
-fi
-cd /TheMovieProviderBot
-pip3 install -U -r requirements.txt
-echo "Starting TheMovieProviderBot...."
+if [ -z $UPSTREAM_REPO ] 
+then 
+  echo "Cloning main Repository" 
+  git clone https://github.com/Itzmeproman/TheMovieProviderBot.git /DQ-The-File-Donor 
+else 
+  echo "Cloning Custom Repo from $UPSTREAM_REPO " 
+  git clone $UPSTREAM_REPO /DQ-The-File-Donor 
+fi 
+cd /DQ-The-File-Donor 
+pip3 install -U -r requirements.txt 
+echo "Starting DQ-The-File-Donor...." 
 python3 bot.py
