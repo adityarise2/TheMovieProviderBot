e69864f6177aeb5f8c895d6eba806405ff488941
itzmeproman <atulpandey3030@gmail.com>
2024-03-09 14:01:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "7007243566:AAHm3PRJlXhOcdeTpm7l5Ab7TmmFZEI
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', "https://graph.org/file/34283e2ca31d427b4e289.jpg")
+PICS = (environ.get('PICS', "https://graph.org/file/34283e2ca31d427b4e289.jpg")).split() 
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/34283e2ca31d427b4e289.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/34283e2ca31d427b4e289.jpg")
 SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/34283e2ca31d427b4e289.jpg")
@@ -34,7 +34,7 @@ STREAM_API = (environ.get('STREAM_API', '96b97324e7c84acdb3070bce5f4b965eada810c
 STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/HowToOpenLinkP/69'))
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6304211335 6651736713').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6258381233').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001800348098 -1002002864063 -1002059268733').split()] #Channel id for auto indexing ( make sure bot is admin )
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '6304211335 6651736713').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
