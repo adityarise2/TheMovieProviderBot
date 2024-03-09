471337d9fbc1b6e0bf2cd73345c3961223b7370b
itzmeproman <atulpandey3030@gmail.com>
2024-03-09 13:35:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,22 +15,22 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '23192666'))
 API_HASH = environ.get('API_HASH', '55c6393f0eee495c2174025f677c36b6')
-BOT_TOKEN = environ.get('BOT_TOKEN', "6843605093:AAH6SOwuMIJFcPryHOmVwEJDt1hZ-UcXFP4")
+BOT_TOKEN = environ.get('BOT_TOKEN', "7007243566:AAHm3PRJlXhOcdeTpm7l5Ab7TmmFZEIqmeo")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://telegra.ph/file/65fe86fc02a73f6fcf0ce.jpg https://telegra.ph/file/6fa70325813885809a64a.jpg https://telegra.ph/file/e06afc1e7abbcd8d4213a.jpg https://telegra.ph/file/3f4040b320d9b7840200a.jpg https://telegra.ph/file/3950fad740fb8ea894df7.jpg')).split() #SAMPLE PIC
-NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
-MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/85d361ab4cb6511006022.mp4")
-SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/86b7b7e2aa7e38f328902.jpg")
-SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://telegra.ph/file/734170f40b8169830d821.jpg'))
-CODE = (environ.get('CODE', 'https://telegra.ph/file/72f425007b22d28bd935e.jpg'))
+PICS = (environ.get('PICS', "https://graph.org/file/34283e2ca31d427b4e289.jpg")
+NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/34283e2ca31d427b4e289.jpg")
+MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/34283e2ca31d427b4e289.jpg")
+SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/34283e2ca31d427b4e289.jpg")
+SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/34283e2ca31d427b4e289.jpg'))
+CODE = (environ.get('CODE', 'https://graph.org/file/34283e2ca31d427b4e289.jpg'))
 
 #stream link shortner
 STREAM_SITE = (environ.get('STREAM_SITE', 'publicearn.com'))
-STREAM_API = (environ.get('STREAM_API', '630f08cf4d4be5c3e979eb8b0de92bb03d07aa27'))
+STREAM_API = (environ.get('STREAM_API', '96b97324e7c84acdb3070bce5f4b965eada810cd')) #yaha tak edited hai bankai
 STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/HowToOpenLinkP/69'))
 
 # Admins, Channels & Users
