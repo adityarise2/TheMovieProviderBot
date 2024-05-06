f5187f3ef0324a89bd4699dd33020cffceae04df
itzmeproman <atulpandey3030@gmail.com>
2024-05-06 13:42:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -62,7 +62,7 @@ HOWTOVERIFY = environ.get('HOWTOVERIFY', default_url) # How to open tutorial lin
 # Others
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shrinkforearn.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', '27bb7f7725c7fbd022cf8cfd6441ac081c8fc765')
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
