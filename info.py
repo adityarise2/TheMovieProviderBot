9d56fdcbe2fb1955b1724a2f246d33af1eb7bb13
Aditya <137521434+adityarise2@users.noreply.github.com>
2024-05-25 21:12:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,7 +55,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "ajnetwork")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Verify
-VERIFY = bool(environ.get('VERIFY', True)) # Verification On ( True ) / Off ( False )
+VERIFY = bool(environ.get('VERIFY', False)) # Verification On ( True ) / Off ( False )
 default_url = 'https://t.me/how_to_open_linkzz/13'
 HOWTOVERIFY = environ.get('HOWTOVERIFY', default_url) # How to open tutorial link for verification
 
